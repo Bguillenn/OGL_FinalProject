07301a37a136113277b7b8034eaa0554c9227502
Brayan Fernando Guillen Nakagawa <bguillenn11@gmail.com>
2022-06-09 21:38:38 -0400

    Creando un triangulo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 //LIBRERIAS OPENGL
 #include <GL/glew.h> //Magia de OpenGL
@@ -16,11 +17,7 @@ int main()
         return -1;
     }
 
-    //Configuracion de la ventana GLFW
-    glfwWindowHint(GLFW_SAMPLES, 4); //Antialiasing
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //OpenGL version mayor
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //OpenGL version menor
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Para que no utilize el viejo OpenGL
+
 
 
     //Creacion de una ventana y su contexto
@@ -41,6 +38,7 @@ int main()
     if(glewInit() != GLEW_OK)
     {
         std::cerr << "Fallo al inicializar GLEW \n";
+        glfwTerminate();
         return -1;
     }
 
@@ -50,16 +48,64 @@ int main()
     glBindVertexArray(VertexArrayID); */
 
     //Arreglo de 3 vectores que representan 3 vertices
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-    };
+    std::vector<glm::vec3> vertices = {
+                                        glm::vec3(0.0f, 0.5f, 0.0f),
+                                        glm::vec3(0.5f, -0.5f, 0.0f),
+                                        glm::vec3(-0.5f, -0.5f, 0.0f)
+                                        };
 
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * 3, &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    //SHADERS
+
+    //Realizamos la creacion de los shader VertexShader y FragmentShader
+    const char* vertexShaderSrc =
+                "#version 400\n"
+                "in vec4 position;"
+                "void main()"
+                "{"
+                     "gl_Position = position;"
+                "}";
+
+    const char* fragmentShaderSrc =
+                "#version 400\n"
+                "out vec4 color;"
+                "void main()"
+                "{"
+                     "color = vec4(0.5, 0.0, 0.0, 1.0);"
+                "}";
+
+    //Creamos los objetos que compilaran los shaders con la funcion CreateShader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //Establecemos el codigo fuente al objeto
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    //Establecemos el codigo fuente al objeto
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
+    glCompileShader(fragmentShader);
+
+
+    //Creamos un objeto de programa donde se uniran todos los shaders
+    GLuint shaderProgram = glCreateProgram();
+
+    //Utilizando la funcion attach unimos los shaders al programa creado
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+
+    //Enlazamos el programa con los shaders e indicamos que sea utilizado
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    //END SHADERS
 
     //Capturamos la tecla escape para cerrar la ventana
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
@@ -67,20 +113,16 @@ int main()
     while(glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && !glfwWindowShouldClose(window))
     {
 
+
+        glClearColor(0.25, 0.25, 0.25, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         //Dibujamos el triangulo
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-                        0,
-                        3,
-                        GL_FLOAT,
-                        GL_FALSE,
-                        0,
-                        (void*)0
-                    );
+
+
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(0);
+
 
         //Intercambiamos buffers y escuchamos eventos de la ventana
         glfwSwapBuffers(window);
